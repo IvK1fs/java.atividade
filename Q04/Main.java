18cc45654d50d1cca42bdd42d49d2c9f6a1375f7
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-26 18:58:38 -0300

    Create Main.java

---
+++ Q04/Main.java
@@ -0,0 +1,27 @@
+package application;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	
+      Scanner sc = new Scanner(System.in);
+      System.out.println("insira as horas: ");
+      
+      int num = sc.nextInt();
+      
+      int num2 = sc.nextInt();
+      
+     int horaDia = 24;
+      int calc;
+      
+      if(num > num2 || num == num2){
+         calc =  horaDia - num + num2;
+          System.out.printf("O jogo durou %d horas!", calc);
+          
+      } else {
+          calc = 24 - (num2 - num - horaDia) * -1 ;
+          System.out.println("O jogo durou %d horas!",calc);
+      }
+        sc.close();
+    }
+}
