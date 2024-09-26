58c794ccd0b5d5d3a8f765f3bb24f6c77b257287
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-09-26 18:29:19 -0300

    Create Main.java

---
+++ Q01/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	
+      Scanner sc = new Scanner(System.in);
+      
+      int num = sc.nextInt();
+      
+      if(num<0 ){
+          
+          System.out.println("negativo");
+          
+      } else {
+          
+          System.out.println("Não negativo");
+      }
+        sc.close();
+    }
+}
